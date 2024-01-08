574c3ef8d3e77fa967cabae65aeff078af789be3
fjpiedade <fernandp.joao@gmail.com>
2024-01-08 20:48:56 +0000

    first commit of payment system application

---
+++ src/test/java/com/fjpiedade/paymentsystem/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjpiedade.paymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
